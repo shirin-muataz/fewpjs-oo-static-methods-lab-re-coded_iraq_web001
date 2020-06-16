870ffbd685bdf1eafdafc4ea24f84d4902c89010
Shirin Muataz <shirinmuataz@gmail.com>
2020-06-16 23:34:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 class Formatter {
   //add static methods here
+  
+  static capitalize(inString){
+   return  inString.charAt(0).toUpperCase();
+  }
+  
+  static sanitize(inString){
+   return  inString.replace(/[^A-Za-z0-9-']+/g,'');
+  }
 }
